813027b84bdee1d0d6508e47afe1f0fa36adea27
dutu <du.tu@hitachivantara.com>
2024-08-08 18:06:33 +0700

    Check text on documnent

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -12,5 +12,5 @@ it("App Router: Works with dynamic route segments", () => {
 
 it('dutu test should be in the document', () => {
   render(<Page params={{ slug: "Test" }} />);
-  expect(screen.getByText('dutu test')).toBeInTheDocument();
+  expect(screen.getByText('dututest')).toBeInTheDocument();
 })
